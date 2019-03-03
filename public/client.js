c505c10ab9ae74afadd4738cb395238743fca1b0
Glitch (hello-express) <none>
2019-03-03 15:29:28 +0000

    ⛴🎎 Checkpoint ./public/client.js:3109048/1418 ./views/index.html:3109048/1018

--- public/client.js
+++ public/client.js
@@ -3,40 +3,11 @@
 
 console.log('hello world :o');
 
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
+// Declaring Scrollmagic controller
+var controller = new ScrollMagic.Controller();
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
-});
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
+//Defining scrollmagic scene
+var scene = new ScrollMagic.Scene({
+  offset: 100, // start scene after scrolling for 100px
+  duration: 400 // pin the element for 400px of scrolling
+})
