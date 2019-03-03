622381c3c54c16884696f1eb6d08d3c4fc7b4a08
Glitch (hello-express) <none>
2019-03-03 17:26:43 +0000

    🌓🎏 Checkpoint ./views/index.html:3109048/115 ./public/client.js:3109048/1120

--- public/client.js
+++ public/client.js
@@ -1,7 +1,25 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
+/* global $,console,document,Handlebars */
+
 console.log('hello world :o');
 
-// init scrollmagic controller
-var controller = new ScrollMagic.Controller(); 
+// MARVEL API
+//default not avail image
+var IMAGE_NOT_AVAIL = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+
+//API Key
+var KEY = process.env.SECRET;
+
+//credit: http://stackoverflow.com/a/1527820/52160
+function getRandomInt (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Get data for Deadpool
+function getDeadpoolData(year) {
+	var url = "https://gateway.marvel.com:443/v1/public/characters/1009268/comics?format=comic&formatType=comic&noVariants=false&limit=5&apikey="+KEY;
+	console.log('nice');
+	return $.get(url);
+}
