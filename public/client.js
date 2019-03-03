7bc6eb02d1ef5f4046469282724e5c66e70e9442
Glitch (hello-express) <none>
2019-03-03 17:37:52 +0000

    📌🍧 Checkpoint ./public/client.js:3109048/418 ./server.js:3109048/2944

--- public/client.js
+++ public/client.js
@@ -3,23 +3,25 @@
 
 /* global $,console,document,Handlebars */
 
-console.log('hello world :o');
+document.addEventListener("DOMContentLoaded", function(){
+  console.log('hello world :o');
 
-// MARVEL API
-//default not avail image
-var IMAGE_NOT_AVAIL = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+  // MARVEL API
+  //default not avail image
+  var IMAGE_NOT_AVAIL = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
 
-//API Key
-var KEY = process.env.SECRET;
+  //API Key
+  var KEY = process.env.SECRET;
 
-//credit: http://stackoverflow.com/a/1527820/52160
-function getRandomInt (min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  //credit: http://stackoverflow.com/a/1527820/52160
+  function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-//Get data for Deadpool
-function getDeadpoolData(year) {
-	var url = "https://gateway.marvel.com:443/v1/public/characters/1009268/comics?format=comic&formatType=comic&noVariants=false&limit=5&apikey="+KEY;
-	console.log('nice');
-	return $.get(url);
-}
+  //Get data for Deadpool
+  function getVenomData() {
+    var url = "https://gateway.marvel.com:443/v1/public/characters/1010787/comics?format=comic&formatType=comic&noVariants=false&limit=5&apikey="+KEY;
+    console.log('venom data loaded');
+    return fetch(url);
+  }
+});
