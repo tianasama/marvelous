c01bd87c350ab6d374c593f20645e446112382d3
Glitch (hello-express) <none>
2019-03-03 16:00:39 +0000

    🍄✌️ Checkpoint ./public/style.css:3109048/226 ./public/client.js:3109048/838 ./views/index.html:3109048/41574

--- public/client.js
+++ public/client.js
@@ -4,22 +4,3 @@
 console.log('hello world :o');
 
 // init
-	var controller = new ScrollMagic.Controller();
-
-// define movement of panels
-	var wipeAnimation = new TimelineMax()
-	.fromTo("container.section.characters", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-	.fromTo("container.section.character1",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-	.fromTo("container.section.character2", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
-  .fromTo("container.section.character3", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
-
-// create scene to pin and link animation
-	new ScrollMagic.Scene({
-	triggerElement: "#pinContainer",
-	triggerHook: "onLeave",
-	duration: "300%"
-								})
-			.setPin("#pinContainer")
-			.setTween(wipeAnimation)
-			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
