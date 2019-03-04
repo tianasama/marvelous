9f65b79f463d1efc8cd31225c4883acc4cf9af24
Glitch (hello-express) <none>
2019-03-04 16:49:27 +0000

    💊🌁 Checkpoint ./public/client.js:3109048/561

--- public/client.js
+++ public/client.js
@@ -2,5 +2,23 @@
 // run by the browser each time your view template is loaded
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  
+  // init
+			var controller = new ScrollMagic.Controller({
+					globalSceneOptions: {
+					triggerHook: 'onLeave'
+						}
+					});
+
+	// get all slides
+				var slides = document.querySelectorAll("section.panel");
+
+	// create scene for every slide
+							for (var i=0; i<slides.length; i++) {
+					new ScrollMagic.Scene({
+										triggerElement: slides[i]
+									})
+									.setPin(slides[i])
+									.addIndicators() // add indicators (requires plugin)
+									.addTo(controller);
+							}
 });
