8c78be85bda51f65b88e53d895d9390624ac098d
Glitch (hello-express) <none>
2019-03-04 18:17:36 +0000

    😾🏮 Checkpoint ./views/index.html:3109048/717 ./public/client.js:3109048/1550

--- public/client.js
+++ public/client.js
@@ -1,33 +1,36 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
+
 document.addEventListener("DOMContentLoaded", function(event) {
+  
+  console.log(ScrollMagic);
   //init scrollmagic
-  var controller = new ScrollMagic.Controller();
+//   var controller = new ScrollMagic.Controller();
   
-  //scenes
-  new ScrollMagic.Scene({
-        triggerElement: "#one",
-        triggerHook: "onLeave",
-    })
-    .setPin("#one")
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
+//   //scenes
+//   new ScrollMagic.Scene({
+//         triggerElement: "#one",
+//         triggerHook: "onLeave",
+//     })
+//     .setPin("#one")
+//     //.addIndicators() // add indicators (requires plugin)
+//     .addTo(controller);
  
-  new ScrollMagic.Scene({
-          triggerElement: "#two",
-          triggerHook: "onLeave",
-      })
-      .setPin("#two")
-      //.addIndicators() // add indicators (requires plugin)
-      .addTo(controller);
+//   new ScrollMagic.Scene({
+//           triggerElement: "#two",
+//           triggerHook: "onLeave",
+//       })
+//       .setPin("#two")
+//       //.addIndicators() // add indicators (requires plugin)
+//       .addTo(controller);
   
-  new ScrollMagic.Scene({
-          triggerElement: "#three",
-          triggerHook: "onLeave",
-      })
-      .setPin("#three")
-      //.addIndicators() // add indicators (requires plugin)
-      .addTo(controller);  
+//   new ScrollMagic.Scene({
+//           triggerElement: "#three",
+//           triggerHook: "onLeave",
+//       })
+//       .setPin("#three")
+//       //.addIndicators() // add indicators (requires plugin)
+//       .addTo(controller);  
   
 });
