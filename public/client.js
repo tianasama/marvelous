2476e507b2e83ac5f245f5bf1dae5a250e692deb
Glitch (hello-express) <none>
2019-03-07 00:29:44 +0000

    🍤🎳 Checkpoint ./public/client.js:3109048/1331

--- public/client.js
+++ public/client.js
@@ -171,25 +171,19 @@ var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Venom", "Deadpool", "Magneto"],
         datasets: [{
-            label: '# of Votes',
+            label: 'How many movies have you appeared in?',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(105, 130, 72, 0.5)',
+                'rgba(221, 63, 69, 0.5)',
+                'rgba(175, 133, 243, 0.5)',
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(34, 35, 36, 1)',
+                'rgba(221, 63, 69, 1)',
+                'rgba(27, 16, 29, 1)',
             ],
             borderWidth: 1
         }]
