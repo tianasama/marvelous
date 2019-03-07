ead113bf68c0f35b915548984ee9ab12aad24d06
Glitch (hello-express) <none>
2019-03-07 00:19:37 +0000

    💗🌚 Checkpoint ./public/style.css:3109048/402 ./public/client.js:3109048/1706 ./views/index.html:3109048/2612

--- public/client.js
+++ public/client.js
@@ -77,6 +77,20 @@ document.addEventListener("DOMContentLoaded", ()=>  {
       //.addIndicators() // for debugging purposes
       .addTo(controller);  
   
+    //Parallax effect - Four
+  new ScrollMagic.Scene({
+        triggerElement: "#four",
+        triggerHook: "onEnter",
+      })
+      .duration(200)
+      .setTween("#four", {
+          backgroundPosition: "100% 100%",
+          ease: Linear.easeNone
+      })
+
+      //.addIndicators() // for debugging purposes
+      .addTo(controller);  
+  
 
 //-------------------------------------------------------------//
 //------------------------- MARVEL API ------------------------//
@@ -146,7 +160,53 @@ document.addEventListener("DOMContentLoaded", ()=>  {
     
     console.log(data);
     
-  });  
+  });
+  
+  
+//-------------------------------------------------------------//
+//------------------------- CHART.JS --------------------------//
+//-------------------------------------------------------------//    
+  
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+  
+  
+  
 });
   
   
